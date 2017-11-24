0ed7d9e7dcc5738eb922f63d5c2f482ad52c0361
Prateek <pratvick@gmail.com>
2017-11-24 14:30:07 +0530

    Create GroceryItemList.jsx file

---
+++ app/components/GroceryItemList.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var createReactClass = require('create-react-class');
+
+module.exports = createReactClass({
+    render:function(){
+        return (
+            <div>
+                <h1>Grocery Listify </h1> {/* Replace it with "Get Feature Build" */}
+            </div>
+        )
+    }
+})
--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,5 @@
-console.log("Hello from JSX!");
+var React = require('react');
+var ReactDOM = require('react-dom');
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+ReactDOM.render(<GroceryItemList/>, app);
