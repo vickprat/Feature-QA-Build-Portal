d56b69d5126af3e6e960ac8f7af6bcd5c023d470
Prateek <pratvick@gmail.com>
2017-12-05 13:40:45 +0530

    Add init.sh to project

---
+++ init.sh
@@ -0,0 +1,11 @@
+branch=master
+git fetch origin
+git checkout $branch
+git reset HEAD --hard
+git pull origin $branch
+echo '[LOG] git pull completed'
+chmod 755 *
+
+npm install
+mongod
+gulp serve
